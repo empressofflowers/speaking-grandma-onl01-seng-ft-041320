e8e4a413dd51f6e34a1eaadc3dc26f2110a88f4f
Devone Austin <devone.austin@gmail.com>
2020-04-14 18:04:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-speak_to_grandma(greeting)
+def speak_to_grandma(greeting)
   if greeting "HUH?! SPEAK UP, SONNY!"
   elsif greeting "NO, NOT SINCE 1938!"
   else greeting.upcase "I LOVE YOU TOO PUMPKIN!"
