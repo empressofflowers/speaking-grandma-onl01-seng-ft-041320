1476368b12a70fd18ba3a731b0c08eb541b49f4c
Devone Austin <devone.austin@gmail.com>
2020-04-14 18:54:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(greeting)
   if greeting.upcase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif greeting.upcase == greeting
+  elsif greeting.capitalize == greeting
     "HUH?! SPEAK UP, SONNY!"
-  else greeting ==
+  else greeting.upcase == greeting
     "NO, NOT SINCE 1938!"
   end
 end
